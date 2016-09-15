09bf98df9615ce8f66b5537a87b679c3f6125d37
vonvick <victor.nwaiwu9@gmail.com>
2016-09-15 12:32:12 +0100

    Initialize the project and making first commit

---
+++ application/views/templates/header.php
@@ -0,0 +1,35 @@
+<?php
+  defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+
+    <title>Morren-Xris Hotels</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+    <link type="text/css" rel="stylesheet" href="views/css/materialize.min.css">
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link type="text/css" rel="stylesheet" href="views/css/style.css">
+
+    <script type="text/javascript" src="views/js/jquery-2.2.4.min.js"></script>
+    <script type="text/javascript" src="views/js/materialize.min.js"></script>
+    <script type="text/javascript" src="views/js/countries.js"></script>
+  </head>
+  <body>
+    <header>
+      <nav class="lime lighten-2">
+        <div class="container">
+          <div class="nav-wrapper">
+            <a href="?controller=pages&action=home" class="brand-logo"> Morren-Xris </a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li><a href="/morren">HOME</a></li>
+              <li><a href="index.php?controller=pages&action=about">ABOUT US</a></li>
+              <li><a href="index.php?controller=pages&action=gallery">GALLERY</a></li>
+              <li><a href="index.php?controller=pages&action=contact">CONTACT US</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
